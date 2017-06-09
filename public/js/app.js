1687ae010a2dfa10d22ad7efe8b346ba49da1e5d
ben <goldenbergben@gmail.com>
2017-06-09 18:08:56 +0300

    started working on dashboard

--- public/js/app.js
+++ public/js/app.js
@@ -1,18 +1,28 @@
 
-var app = angular.module('ganim', ['auth'])
-.config(function($stateProvider) {
-    var helloState = {
-        name: 'hello',
-        url: '/hello',
-        template: '<h3>hello world!</h3>'
-    }
+var app = angular.module('ganim', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+    var addState = {
+        name: 'add',
+        url: '/add',
+        templateUrl: 'templates/addClient'
+    };
+
+    var welcome = {
+        name: 'welcome',
+        url: 'welcome',
+        templateUrl: 'templates/welcome.html'
+    };
 
     var aboutState = {
         name: 'about',
         url: '/about',
         template: '<h3>Its the UI-Router hello world app!</h3>'
-    }
+    };
 
-    $stateProvider.state(helloState);
+    $stateProvider.state(welcome);
     $stateProvider.state(aboutState);
+    $urlRouterProvider.otherwise('/welcome');
+
+
 });
