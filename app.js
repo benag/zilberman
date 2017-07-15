4fac1ea24a391f1c4a0e59b01092f1ee5194cf49
ben <goldenbergben@gmail.com>
2017-07-15 10:42:12 +0300

    added/

--- app.js
+++ app.js
@@ -111,7 +111,7 @@ app.post('/profile', upload.single('file'), function (req, res, next) {
     if (!req.file){ //work around file was not moved
         let file = req.files.file;
         let oldPath  = file.path;
-        let newPath = './public/uploads' + oldPath.split('/').pop();
+        let newPath = './public/uploads/' + oldPath.split('/').pop();
         fs.rename(oldPath, newPath, function (err) {
             if (err) throw err
             console.log('Successfully renamed - AKA moved!')
