e3fdc29ea99fb5195192338e36cc9f070737d91e
ben <goldenbergben@gmail.com>
2017-11-30 13:56:45 +0200

    added back button

--- public/js/controllers/headerCtr.js
+++ public/js/controllers/headerCtr.js
@@ -1,6 +1,11 @@
 angular.module('ganim').controller('headerCtrl', ['$scope', '$stateParams', '$location', '$state', '$rootScope', 'global',
     function($scope, $stateParams, $location, $state, $rootScope, global) {
 
+        $scope.$state = $state;
+
+        $scope.returnBack = function(){
+            $state.go('products');
+        }
         $scope.logOut = () => {
             global.logOutUser();
             $state.go('login');
