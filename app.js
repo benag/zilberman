af5c275d0c7b76c89779f83ee5aadf3be682f199
ben <goldenbergben@gmail.com>
2018-02-06 20:59:31 +0200

    sadads

--- app.js
+++ app.js
@@ -219,7 +219,7 @@ app.post('/profile', upload.single('file'), function (req, res, next) {
 
 });
 
-app.post('/save', (req, res) => {
+app.post('save', (req, res) => {
     newEntry.save(req.body.form);
 });
 
