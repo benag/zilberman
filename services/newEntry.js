71d4e4f08804f6e77d7ddb1e1b297502e708eb0b
ben <goldenbergben@gmail.com>
2018-02-07 13:36:02 +0200

    asd

--- services/newEntry.js
+++ services/newEntry.js
@@ -21,13 +21,13 @@ class newEntry {
         //if (client.recordset.length > 0) { //client exist already
 
         //}else{// new client
-            let cTaz1 = form.id || '', cTaz2 = form.mate.id || '23', cName = form.firstName || '', cFamily = form.lastName || '', cGender = form.gender || '',
-                cMobile = form.mobile || '234', cPhone = form.phone || '234', cEmail = form.email || 'goldenbergben@gmail.com', cBDate = Date.now() || '',
+            let cTaz1 = form.id || '', cTaz2 = form.mate.id || '', cName = form.firstName || '', cFamily = form.lastName || '', cGender = form.gender || '',
+                cMobile = form.mobile || '', cPhone = form.phone || '', cEmail = form.email || '', cBDate = Date.now() || '',
                 cTazDate= form.iddate || '', cRemark ='' || '', cSmoke = 0 || '', cQuitSmokeDate = '' || '';
             //let insert = `INSERT INTO tClients (cTaz1, cTaz2, cName, cFamily, cGender, cMobile, cPhone, cEmail,cBDate, cTazDate, cRemark, cSmoke, cQuitSmokeDate)
             //    VALUES ( ${cTaz1} , ${cTaz2}, ${cName}, ${cFamily}, ${cGender} , ${cMobile}, ${cPhone}, ${cEmail}, ${cTazDate}, ${cRemark}, ${cSmoke}, ${cQuitSmokeDate} )`;
-            let insert = `INSERT INTO tClients (cTaz1, cTaz2, cName, cFamily, cGender, cMobile, cPhone, cEmail, cBDate)
-                VALUES ( '${cTaz1}' , '${cTaz2}', '${cName}', '${cFamily}', 1 , '${cMobile}','${cPhone}','${cEmail}',${cTazDate} )`;
+            let insert = `INSERT INTO tClients (cTaz1, cTaz2, cName, cFamily, cGender, cMobile, cPhone, cEmail, cBDate, cTazDate,cRemark, cSmoke, cQuitSmokeDate)
+                VALUES ( '${cTaz1}' , '${cTaz2}', '${cName}', '${cFamily}', 1 , '${cMobile}','${cPhone}','${cEmail}', ${cBDate}, ${cTazDate}, ${cRemark}, ${cSmoke}, ${cQuitSmokeDate} )`;
             let newClient = await this.sql.query(insert);
         //}
         console.log(gender);
