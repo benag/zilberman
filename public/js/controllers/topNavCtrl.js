2388ff8fcd428ac025d7bb476851c990eac18e49
ben <goldenbergben@gmail.com>
2018-02-08 08:07:14 +0200

    asd

--- public/js/controllers/topNavCtrl.js
+++ public/js/controllers/topNavCtrl.js
@@ -1,6 +1,8 @@
 angular.module('ganim').controller('topNavCtrl', ['$scope', '$stateParams', '$location', '$state',
     function($scope, $stateParams, $location, $state) {
 
+        let domain = $location.domain();
+        console.log(domain);
         $scope.state = (state) => {
             window.location.href = 'http://18.221.178.131:3000/index.html#!/' + state;
             //$state.go('summery');
