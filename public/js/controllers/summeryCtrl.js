d07f9a97e6509f766de93e84cf89ab7630db756c
ben <goldenbergben@gmail.com>
2018-02-14 22:43:03 +0200

    asdds

--- public/js/controllers/summeryCtrl.js
+++ public/js/controllers/summeryCtrl.js
@@ -1,8 +1,9 @@
 angular.module('ganim').controller('summeryCtrl', ['$scope', '$stateParams', '$location', '$state','$http', 'global',
     function($scope, $stateParams, $location, $state, $http, global) {
-
+        $scope.loading = true;
         $http.get('/products')
         .then( (data) => {
+            $scope.loading = false;
             $scope.products = data.data;
         })
         $scope.goToClient = (index) => {
