c5167a3aa4e595fd0158b0d7b150f9664b2a14a8
ben <goldenbergben@gmail.com>
2018-02-18 22:10:14 +0200

    asdad

--- public/js/controllers/topNavCtrl.js
+++ public/js/controllers/topNavCtrl.js
@@ -3,7 +3,7 @@ angular.module('ganim').controller('topNavCtrl', ['$scope', '$stateParams', '$lo
 
         
         $scope.state = (state) => {
-            $state.go(state);
+            $state.go(state,{ reload: true });
         };
 
         $scope.ifstate = (state) => {
