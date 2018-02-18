7938c462b025d468bf223201007070f9f475b50f
ben <goldenbergben@gmail.com>
2018-02-18 16:06:30 +0200

    asd

--- public/js/controllers/carModalCtrl.js
+++ public/js/controllers/carModalCtrl.js
@@ -1,13 +1,14 @@
 angular.module('ganim').controller('carModalCtrl', function ($uibModalInstance, $scope, items, car) {
     
     $scope.items = items;
+    $scope.car = car;
     $scope.selected = {
       item: $scope.items[0]
     };
   
-    $scope.ok = function () {
-      $uibModalInstance.close($scope.selected.item);
-    };
+    // $scope.ok = function () {
+    //   $uibModalInstance.close($scope.selected.item);
+    // };
   
     $scope.cancel = function () {
       $uibModalInstance.dismiss('cancel');
