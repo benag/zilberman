3a59514383b6c8938b27c2940fb21f7085e7ecde
ben <goldenbergben@gmail.com>
2018-02-21 08:28:33 +0200

    users update

--- public/js/controllers/newEntryCtrl.js
+++ public/js/controllers/newEntryCtrl.js
@@ -78,6 +78,7 @@ angular.module('ganim').controller('newEntryCtrl', ['$scope', '$stateParams', '$
                 $scope.form.insuranceForm.cars.push(p.cars);
                 let date = moment($scope.form.insuranceForm.cars[0].carRenewDate);
                 $scope.form.insuranceForm.cars[0].carRenewDate = date.format('DD/MM/YYYY');
+                $scope.form.insuranceForm.cars[0].carTypeID = String($scope.form.insuranceForm.cars[0].carTypeID);
                 
                 // $scope.form.insuranceForm.cars.forEach( (car) => { 
                 //     let date = moment(car.carRenewDate);
